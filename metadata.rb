3540bbaaeb80f4ebdad14a7bed159fd3c6fce5f8
Darron Froese <darron@froese.org>
2014-03-16 18:03:08 -0600

    Adding ChefSugar.

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,5 @@ license          'Apache 2.0'
 description      'Installs/configures skeleton'
 version          '0.1.0'
 recipe           'skeleton::default', 'Installs/configures skeleton'
+
+depends 'chef-sugar'
